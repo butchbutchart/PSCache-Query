bc6c105d7b07127438321d3aebf94b46eb93fc0c
butchbutchart <42536650+butchbutchart@users.noreply.github.com>
2023-08-09 14:29:06 +0100

    Update cachequery.ps1
    need to add if/else

--- cachequery.ps1
+++ cachequery.ps1
@@ -33,7 +33,9 @@ Write-Host "These breakglass accounts are available in the cache:"
 # Print specific fields from the response with aligned columns
 $listAccountsResponse | ConvertFrom-Csv -Delimiter "`t" | Format-Table -Property SystemName, AccountName, DomainName -AutoSize
 
-$listSecretsResponse | ConvertFrom-Csv -Delimiter "`t" | Format-Table -Property Title, Description -AutoSize
+$listSecretsResponse | ConvertFrom-Csv -Delimiter "`t" | Format-Table -Property Title, Description, Id -AutoSize
+
+### If the request is for a managed system ####
 
 Write-Host
 
@@ -83,7 +85,55 @@ Password: $response
     Write-Host "#######"
     Write-Host "#######"
 }
-# Your script code goes here
+
+### If the request is for a secret ####
+
+Write-Host
+
+# Get ManagedSystem
+$SecretID = Read-Host "Enter the ID from above of the secret you want to request:"
+Write-Host
+
+
+# Construct command
+$commandsecret = ".\psrun2 -i 127.0.0.1:8443 $apiKey $apiUser GET Secrets-Safe/Secrets/$SecretID
+
+# Execute command and capture output
+$response = Invoke-Expression $commandsecret
+
+# Print response with headings
+Write-Host 
+Write-Host "ManagedSystem: $managedSystem ManagedAccount: $managedAccount"
+Write-Host
+Write-Host -ForegroundColor Red "Password: $response"
+Write-Host
+
+# Store output as text file
+$storeOutput = Read-Host "Would you like to store the output as a text file? (yes/no)"
+if ($storeOutput.ToLower() -eq "yes") {
+    $date = Get-Date -Format "yyyyMMdd"
+    $time = Get-Date -Format "HHmmss"
+    $outputPath = "$env:USERPROFILE\Desktop\breakglass-password$date$time.txt"
+    $outputContent = @"
+Managed System: $managedSystem
+Requested Account: $managedAccount
+Password: $response
+"@
+    $outputContent | Out-File -FilePath $outputPath -Encoding UTF8
+
+    Write-Host "#######"
+    Write-Host "#######"
+    Write-Host "#######"
+    Write-Host "Output has been stored as a text file: $outputPath"
+    Write-Host "#######"
+    Write-Host "#######"
+    Write-Host "#######"
+    Write-Host "#######"
+    Write-Host -ForegroundColor Red "Remember to delete this file if account is not being rotated"
+    Write-Host "#######"
+    Write-Host "#######"
+    Write-Host "#######"
+}
 
 # Pause before exiting
 Write-Host "Press Enter to exit..."
