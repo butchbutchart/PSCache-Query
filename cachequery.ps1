dbd049992a338a0a5d0f45e68986fd3d4cc994eb
butchbutchart <42536650+butchbutchart@users.noreply.github.com>
2023-08-16 11:20:10 +0100

    Update cachequery.ps1

--- cachequery.ps1
+++ cachequery.ps1
@@ -35,103 +35,92 @@ $listAccountsResponse | ConvertFrom-Csv -Delimiter "`t" | Format-Table -Property
 
 $listSecretsResponse | ConvertFrom-Csv -Delimiter "`t" | Format-Table -Property Title, Description, Id -AutoSize
 
-### If the request is for a managed system ####
+### If the user wants to request an account or a secret ####
 
-Write-Host
+# Print available options
+Write-Host "Do you want to request a managed account or a secret?"
+Write-Host "1. Managed Account"
+Write-Host "2. Secret"
 
-# Get ManagedSystem
-$managedSystem = Read-Host "Enter the system from which you want to request an account:"
-Write-Host
+# Get user's choice
+$userChoice = Read-Host "Enter the number of your choice (1 or 2):"
 
-# Get ManagedAccount
-$managedAccount = Read-Host "Enter the name of the account you would like to request:"
+# Process user's choice
+if ($userChoice -eq "1") {
+    ### If the request is for a managed system ####
 
-# Construct command
-$command = ".\psrun2 -i 127.0.0.1:8443 $apiKey $apiUser RetrievePassword $managedSystem $managedAccount 'Break-glass Request'"
+    Write-Host
 
-# Execute command and capture output
-$response = Invoke-Expression $command
+    # Get ManagedSystem
+    $managedSystem = Read-Host "Enter the system from which you want to request an account:"
+    Write-Host
 
-# Print response with headings
-Write-Host 
-Write-Host "ManagedSystem: $managedSystem ManagedAccount: $managedAccount"
-Write-Host
-Write-Host -ForegroundColor Red "Password: $response"
-Write-Host
+    # Get ManagedAccount
+    $managedAccount = Read-Host "Enter the name of the account you would like to request:"
+    
+    # Construct command
+    $command = ".\psrun2 -i 127.0.0.1:8443 $apiKey $apiUser RetrievePassword $managedSystem $managedAccount 'Break-glass Request'"
 
-# Store output as text file
-$storeOutput = Read-Host "Would you like to store the output as a text file? (yes/no)"
-if ($storeOutput.ToLower() -eq "yes") {
-    $date = Get-Date -Format "yyyyMMdd"
-    $time = Get-Date -Format "HHmmss"
-    $outputPath = "$env:USERPROFILE\Desktop\breakglass-password$date$time.txt"
-    $outputContent = @"
+    # Execute command and capture output
+    $response = Invoke-Expression $command
+
+    # Print response with headings
+    Write-Host 
+    Write-Host "ManagedSystem: $managedSystem ManagedAccount: $managedAccount"
+    Write-Host
+    Write-Host -ForegroundColor Red "Password: $response"
+    Write-Host
+
+    # Store output as text file
+    $storeOutput = Read-Host "Would you like to store the output as a text file? (yes/no)"
+    if ($storeOutput.ToLower() -eq "yes") {
+        $date = Get-Date -Format "yyyyMMdd"
+        $time = Get-Date -Format "HHmmss"
+        $outputPath = "$env:USERPROFILE\Desktop\breakglass-password$date$time.txt"
+        $outputContent = @"
 Managed System: $managedSystem
 Requested Account: $managedAccount
 Password: $response
 "@
-    $outputContent | Out-File -FilePath $outputPath -Encoding UTF8
+        $outputContent | Out-File -FilePath $outputPath -Encoding UTF8
 
-    Write-Host "#######"
-    Write-Host "#######"
-    Write-Host "#######"
-    Write-Host "Output has been stored as a text file: $outputPath"
-    Write-Host "#######"
-    Write-Host "#######"
-    Write-Host "#######"
-    Write-Host "#######"
-    Write-Host -ForegroundColor Red "Remember to delete this file if account is not being rotated"
-    Write-Host "#######"
-    Write-Host "#######"
-    Write-Host "#######"
-}
+        Write-Host "#######"
+        Write-Host "#######"
+        Write-Host "#######"
+        Write-Host "Output has been stored as a text file: $outputPath"
+        Write-Host "#######"
+        Write-Host "#######"
+        Write-Host "#######"
+        Write-Host "#######"
+        Write-Host -ForegroundColor Red "Remember to delete this file if the account is not being rotated"
+        Write-Host "#######"
+        Write-Host "#######"
+        Write-Host "#######"
+    }
+    
+} elseif ($userChoice -eq "2") {
+    ### If the request is for a secret ####
 
-### If the request is for a secret ####
+    Write-Host
 
-Write-Host
+    # User submits secret ID
+    $SecretID = Read-Host "Enter the ID from above of the secret you want to request:"
+    Write-Host
 
-# User submits secret ID
-$SecretID = Read-Host "Enter the ID from above of the secret you want to request:"
-Write-Host
+    # Construct command
+    $commandsecret = ".\psrun2 -i 127.0.0.1:8443 $apiKey $apiUser GET Secrets-Safe/Secrets/$SecretID"
+
+    # Execute command and capture output
+    $secretresponse = Invoke-Expression $commandsecret
+
+    # Print response with headings
+    Write-Host 
+    Write-Host "Secret: $SecretID"
+    Write-Host
+    $secretresponse | ConvertFrom-Csv -Delimiter "`t" | Format-Table -Property Title, Description, Password -AutoSize
+    Write-Host
 
 
-# Construct command
-$commandsecret = ".\psrun2 -i 127.0.0.1:8443 $apiKey $apiUser GET Secrets-Safe/Secrets/$SecretID
-
-# Execute command and capture output
-$secretresponse = Invoke-Expression $commandsecret
-
-# Print response with headings
-Write-Host 
-Write-Host "Secret: $SecretID"
-Write-Host
-Write-Host -ForegroundColor Red "Password: $secretresponse"
-Write-Host
-
-# Store output as text file
-$storeOutputSecret = Read-Host "Would you like to store the output as a text file? (yes/no)"
-if ($storeOutputSecret.ToLower() -eq "yes") {
-    $date = Get-Date -Format "yyyyMMdd"
-    $time = Get-Date -Format "HHmmss"
-    $outputPathSecret = "$env:USERPROFILE\Desktop\breakglass-password$date$time.txt"
-    $outputContent = @"
-Secret ID: $SecretID
-Secret: $secretresponse
-"@
-    $outputContent | Out-File -FilePath $outputPath -Encoding UTF8
-
-    Write-Host "#######"
-    Write-Host "#######"
-    Write-Host "#######"
-    Write-Host "Output has been stored as a text file: $outputPath"
-    Write-Host "#######"
-    Write-Host "#######"
-    Write-Host "#######"
-    Write-Host "#######"
-    Write-Host -ForegroundColor Red "Remember to delete this file if account is not being rotated"
-    Write-Host "#######"
-    Write-Host "#######"
-    Write-Host "#######"
 }
 
 # Pause before exiting
